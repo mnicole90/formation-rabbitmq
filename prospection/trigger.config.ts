ba4a54e54acaf43a219ffd5daf5c8667e295a632
Maxime N. <72199293+mnicole90@users.noreply.github.com>
2026-07-13 12:31:52 +0200

    chore: initialize Trigger.dev project

---
+++ prospection/trigger.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "@trigger.dev/sdk";
+
+export default defineConfig({
+  project: "proj_xojrshpefiznkcpdsodk",
+  runtime: "node",
+  logLevel: "log",
+  maxDuration: 3600,
+  dirs: ["./src/trigger"],
+});
